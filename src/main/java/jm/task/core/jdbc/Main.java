851c8160165334191769a34366a3ec402a64cb36
svtart <promixmaxads@gmail.com>
2022-09-01 15:47:55 +0300

    Finish Util

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        Util util = new Util();
+        util.getConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public Connection getConnection() {
+        Connection connection = null;
+        try{
+            String url = "jdbc:mysql://localhost:3306/KataSpringTask1";
+            String username = "root";
+            String password = "12345678";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+
+                System.out.println("Connection to Store DB succesfull!");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        } return connection;
+    }
 }
