26018549ee29d24bb11289dc4c22c1d47e834eeb
svtart <promixmaxads@gmail.com>
2022-09-02 15:10:51 +0300

    Final version (fixed)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,18 +5,16 @@ import java.sql.*;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    public Connection getConnection() {
+
+    public static Connection getConnection() {
         Connection connection = null;
+        String URL = "jdbc:mysql://localhost:3306/KataSpringTask1";
+        String USERNAME = "root";
+        String PASSWORD = "12345678";
         try{
-            String url = "jdbc:mysql://localhost:3306/KataSpringTask1";
-            String username = "root";
-            String password = "12345678";
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-
-            try (Connection conn = DriverManager.getConnection(url, username, password)){
-
-                System.out.println("Connection to Store DB succesfull!");
-            }
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection to Store DB succesfull!");
         }
         catch(Exception ex){
             System.out.println("Connection failed...");
